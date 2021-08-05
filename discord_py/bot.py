62f5b05ca259f8ebe443b4f0abf663c7b1a08866
T0b1e <77008784+T0b1e@users.noreply.github.com>
2021-08-05 23:23:26 +0700

    Update bot.py

--- discord_py/bot.py
+++ discord_py/bot.py
@@ -1,23 +1,81 @@
 import discord
-from discord import channel
-from discord import member
+from discord.embeds import Embed
 from discord.ext import commands
-from discord.player import FFmpegPCMAudio #audio
+from discord.player import FFmpegPCMAudio
 from discord.utils import get
-import youtube_dl #play
-import os #play
-import datetime #gacha,userinfo
+import youtube_dl #play url
+import os #play os
+import datetime #set time
 import asyncio #gacha
-import random #gacha
+import random #gacha prize
+import json #json
 
-
-client = commands.Bot(command_prefix='=')
+client = commands.Bot(command_prefix= '=')
 client.remove_command("help")
+
+#Cogs
+""" @client.command()
+async def load(ctx,extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx,extension):
+    client.unload_extension(f'cogs.{extension}')
+
+@client.command()
+async def reload(ctx,extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}') #cut .py 3 char """
+
 
 @client.event
 async def on_ready(): 
     await client.change_presence(status=discord.Status.idle,activity=discord.Game('/help'))
     print('Bot is now ready')
+
+@client.command() #list commands
+async def list(ctx):
+    embed = discord.Embed(title = "List commands", description = "Use '=list'",color = ctx.author.color)
+    embed.add_field(name = "Json",value=
+    "TOBI but in json file"
+    "**Network**        for Check TOBI Network (.ms)\n"
+    "**userinfo**       for Check user information (Join server, Role)\n"
+    "**gacha _ _**      for random number from fisrt to second\n"
+    "**prize _ _**      for Give away prize from Admin by set time and prize\n"
+    "**poll**           for vote Good or Bad (No limit user)\n"
+    "**spawn**          for spawn TOBI to channel\n")
+    embed.add_field(name= "Math",value=
+    "**plus _ _**       for plus the numbers\n"
+    "**minus _ _**      for minus the numbers\n"
+    "**multiplie _ _**  for multiplie the numbers\n"
+    "**divide __**     for divide the numbers\n")
+    embed.add_field(name= "Audio",value=
+    "**play 'url'**     for play song\n"
+    "**stop**           for stop song\n"
+    "**pause**          for pause song\n"
+    "**resume**         for resume song\n" )
+    embed.add_field(name= "ADMIN",value=
+    "only Admin or mod can acess this permission"
+    "**mute @___**      for muted someone\n"
+    "**unmute @___**    for unmuted someone\n"
+    "**kick @___**      for kick someone from the server\n"
+    "**ban @___**       for ban someone from the server\n"
+    "**banvote**        for ban Someone but use Vote from Admin\n"
+    "**kickvote**       for kick Someone but use Vote from Admin\n")
+    embed.add_field(name= "Tobi",value=
+    "**tobiinfo**       for TOBI information\n"
+    "**git**            for GitHub\n"
+    "**updateinfo**     for TOBI update command or function\n")
+    await ctx.send(embed = embed)
+
+@client.command(description="Gets the bot's latency.")
+async def Network(ctx): #Network
+    latency = round(client.latency * 1000, 1)
+    await ctx.send(f"Network = {latency}ms")
 
 @client.event
 async def on_member_join(member): # join
@@ -27,7 +85,7 @@ async def on_member_join(member): # join
 async def on_member_remove(member): # remove
     print(f'{member} has removed the server.')
 
-@client.command()
+@client.command() #userinfo
 async def userinfo(ctx, member:discord.Member):
     roles = [role for role in member.roles]
     embed = discord.Embed(title = f"Userinfo{member.name}",color = ctx.author.color)
@@ -57,13 +115,58 @@ async def multiplie(ctx,a: float,b: float):
 async def divide(ctx,a: float,b: float):
     await ctx.send(a / b)
 
-@client.command()
+@client.command() # multiplie _ _
+async def test(ctx,a: float):
+    if a > 100:
+        await ctx.send("pass")
+    else:
+        await ctx.send("Try again")
+
+@client.command() #Kick
 async def kick(ctx, member :discord.Member, *,reason = "Kick because you don't follow the rules"):
     await member.kick(reason=reason)
 
-@client.command()
+@client.command() #Ban
 async def ban(ctx, member :discord.Member, *,reason = "Ban because you don't follow the rules"):
     await member.ban(reason=reason)
+
+@client.command(description="Mutes the specified user.") #Mute
+@commands.has_permissions(manage_messages=True)
+async def mute(ctx, member: discord.Member,*, reason=None):
+    guild = ctx.guild
+    mutedRole = discord.utils.get(guild.roles, name="Muted")
+
+    if not mutedRole:
+        mutedRole = await guild.create_role(name="Muted")
+
+        for channel in guild.channels:
+            await channel.set_permissions(mutedRole, speak=False, send_messages=True, read_message_history=False, read_messages=False)
+
+    await member.add_roles(mutedRole, reason=reason)
+    await ctx.send(f"Muted {member.mention} for reason {reason}")
+    await member.send(f"You were muted in the server {guild.name} for {reason}")
+
+@client.command(description="Unmutes a specified user.") #Unmute
+@commands.has_permissions(manage_messages=True)
+async def unmute(ctx, member: discord.Member):
+    mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
+
+    await member.remove_roles(mutedRole)
+    await ctx.send(f"Unmuted {member.mention}")
+    await member.send(f"You were unmuted in the server {ctx.guild.name}")
+
+@client.command(invoke_without_command = True) #tobiinfo
+async def json(ctx):
+    data = {
+    "name": "TOBI",
+    "JOB": "Assistance",
+    "Version": "0.6.1",
+    "Created": "12/01/2019",
+    "Last Update": "5/8/2021",
+    "Library": "discord, datetime, asyncio, random, math, os",
+    "command": "28 commands"
+    }
+    await ctx.send(data)
 
 @client.command(invoke_without_command = True) #tobiinfo
 async def tobiinfo(ctx):
@@ -80,12 +183,14 @@ async def updateinfo(ctx):
     em = discord.Embed(title = "TOBI Update information", description = "Use '=updateinfo'",color = ctx.author.color)
     em.add_field(name = "Info",value=
     "**Prefix change from / to = **\n"
+    "**Spawn**\n"
     "**Music command** = play,stop,pause,resume\n"
     "**Calculater Mode** = plus,minus,multiplie,divide\n"
     "**Member cout**\n"
     "**UserInfo**\n"
     "**Voting, Poll** \n"
     "**Gacha** = number,member\n"
+    "**Mute** = mute,unmute\n"
     "**Coming soon** function = queue, skip, Homework notification, Uptime Classroom")
     em.set_footer(text="Version 0.5.1 (5/8/2021)")
     await ctx.send(embed = em)
@@ -105,7 +210,7 @@ async def helpplus(ctx):
     em = discord.Embed(title = "plus", description = "Use '=plus <Number>_<Number>'",color = ctx.author.color)
     await ctx.send(embed = em)
 
-@client.command() #githup repo
+@client.command() #git
 async def git(ctx):
     em = discord.Embed(title = "Github repo", description = "Use '=git'",color = ctx.author.color)
     em.add_field(name = "Github",value="https://github.com/T0b1e/Discord.tob.git")
@@ -119,7 +224,7 @@ async def poll(ctx,*,message):
     await msg.add_reaction('👍')
     await msg.add_reaction('👎')
 
-@client.command() #vote for ban member
+@client.command() #Vote for ban
 async def banvote(ctx, member :discord.Member,message):
     Yes = True
     No = False
@@ -181,7 +286,7 @@ async def play(ctx, url : str):
         await ctx.send("Wait for the current playing music to end or use the 'stop' command")
         return
 
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name= "ห้องเมียแขกและเกาหลี")
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name= "General")#"ห้องเมียแขกและเกาหลี"
     await voiceChannel.connect()
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
 
@@ -231,5 +336,5 @@ async def resume(ctx):
 async def stop(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     voice.stop()
-    #await ctx.send("**stop**")
+    #await ctx.send("**stop**") 
 client.run('ODMzMjgwODM0MTYwOTUxMjk2.YHwDQA.6yR2YXO4fI3HoLdYTeC18J8NYE8')
